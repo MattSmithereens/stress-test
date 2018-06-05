6e6ca4debc0a58d16ed7c7f738fe20bdb1ed4ad3
Abel Trotter and Matt Smith <abel@>
2018-06-05 14:43:57 -0700

    Remove comment, show gif for each result, adjust show/hide ids

--- js/scripts.js
+++ js/scripts.js
@@ -22,17 +22,29 @@ function parseTotals(warningVal, symptomsVal, copingVal) {
 }
 
 function displayResults(score) {
-  $("#results").fadeToggle(800);
-  $("#stressTest").hide();
+  resultsShowAndHide();
   if (score <= MIN_CAP) {
-    $("#results").text("You are doing great! Keep it up!");
+    $("#scoreResults").text("You are doing great! Keep it up!");
+    $(".results-image").append(imageUrl("happy.gif"));
   } else if (score > MIN_CAP && score <= MID_CAP) {
-    $("#results").text("You are doing okay, but here are some helpful tips...");
+    $("#scoreResults").text("You are doing okay, but here are some helpful tips...");
+    $(".results-image").append(imageUrl("meh.gif"));
   } else if (score > MID_CAP) {
-    $("#results").text("You should seek help immediately. Please see resources below.");
+    $("#scoreResults").text("You should seek help immediately. Please see resources below.");
+    $(".results-image").append(imageUrl("send-help.gif"));
   } else {
     alert("Stop trying to break things...");
   }
+}
+
+function resultsShowAndHide() {
+  $("#surveyResults").fadeToggle(800);
+  $("#stressTest").hide();
+}
+
+function imageUrl(imgName) {
+  console.log(imgName);
+  return "<img src='img/" + imgName + "' alt='An image of your results'>";
 }
 
 $(document).ready(function() {
